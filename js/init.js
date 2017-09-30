b86455507e17075d6442fa381c0922701a57091b
Cathy Miller <cat.miller07.cm@gmail.com>
2017-09-29 22:40:43 -0400

    now using MaterializeCss

---
+++ js/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+/** photo carousel **/
+$(document).ready(function(){
+    $('.carousel').carousel();
+   /**$('.carousel.carousel-slider').carousel({fullWidth: true});**/
+});
+
+
